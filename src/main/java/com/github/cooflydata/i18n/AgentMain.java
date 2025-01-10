a2fc51b8f07f318a9b0f99e4222566ae9168efe1
donglai <80871901@qq.com>
2025-01-10 18:33:16 +0800

    开发 javaagent

--- src/main/java/com/github/cooflydata/i18n/AgentMain.java
+++ src/main/java/com/github/cooflydata/i18n/AgentMain.java
@@ -5,12 +5,32 @@ import javassist.CtClass;
 import javassist.CtField;
 import javassist.CtMethod;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AgentMain {
+
+    private static final String LOG_FILE_NAME = "D:\\runtime-KNIME\\.metadata\\i18n.log";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void log(String message) {
+        try (FileWriter fileWriter = new FileWriter(LOG_FILE_NAME, true);
+             PrintWriter printWriter = new PrintWriter(fileWriter)) {
+            String logMessage = String.format("%s %s%n", dateFormat.format(new Date()),message);
+            printWriter.println(logMessage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("Eclipse Agent started.");
         inst.addTransformer(new ClassFileTransformer() {
@@ -19,26 +39,57 @@ public class AgentMain {
                                     ProtectionDomain protectionDomain, byte[] classfileBuffer)
                     throws IllegalClassFormatException {
                 try {
-                    if (className.equals("org/eclipse/swt/widgets/Label") || className.equals("org/eclipse/swt/widgets/MenuItem")) {
-                        CtClass clazz = null;
-                        clazz = new ClassPool(true).get(className.replace('/', '.'));
+                    if ("org/eclipse/swt/widgets/Label".equals(className) || "org/eclipse/swt/widgets/MenuItem".equals(className)) {
 
-                        StringBuilder loggerDeclaration = new StringBuilder();
-                        loggerDeclaration.append("private static final java.util.logging.Logger logger = ");
-                        loggerDeclaration.append("java.util.logging.Logger.getLogger(\"").append(clazz.getName()).append("\");\n");
-                        clazz.addField(CtField.make(loggerDeclaration.toString(), clazz));
+                        ClassPool pool = ClassPool.getDefault();
+
+                        log("1111 " + className);
+
+                        CtClass clazz = null;
+                        clazz = pool.get(className.replace('/', '.'));
+
+                        log("22222" + clazz.getName());
+
+                        clazz.addField(CtField.make("private static final String LOG_FILE_NAME = \"D:\\runtime-KNIME\\.metadata\\i18n.log\"", clazz));
+                        clazz.addField(CtField.make("private static final SimpleDateFormat dateFormat = new SimpleDateFormat(\"yyyy-MM-dd HH:mm:ss\")", clazz));
+
+                        clazz.addMethod(CtMethod.make("public static void log(String message) {\n" +
+                                "        try (FileWriter fileWriter = new FileWriter(LOG_FILE_NAME, true);\n" +
+                                "             PrintWriter printWriter = new PrintWriter(fileWriter)) {\n" +
+                                "            String logMessage = String.format(\"%s %s%n\", dateFormat.format(new Date()),message);\n" +
+                                "            printWriter.println(logMessage);\n" +
+                                "        } catch (IOException e) {\n" +
+                                "            throw new RuntimeException(e);\n" +
+                                "        }\n" +
+                                "    }", clazz));
+
+//                        StringBuilder loggerDeclaration = new StringBuilder();
+//                        loggerDeclaration.append("private static final java.util.logging.Logger logger = ");
+//                        loggerDeclaration.append("java.util.logging.Logger.getLogger(\"").append(clazz.getName()).append("\");\n");
+//                        clazz.addField(CtField.make(loggerDeclaration.toString(), clazz));
+
+                        // Declare and initialize Logger instance
+//                        StringBuilder loggerDeclaration = new StringBuilder();
+//                        loggerDeclaration.append("private static final org.slf4j.Logger logger = ");
+//                        loggerDeclaration.append("org.slf4j.LoggerFactory.getLogger(\"").append(clazz.getName()).append("\");\n");
+//                        clazz.addField(CtField.make(loggerDeclaration.toString(), clazz));
 
 
                         CtMethod setTextMethod = clazz.getDeclaredMethod("setText");
                         StringBuilder src = new StringBuilder();
                         src.append("{\n");
-                        src.append("    logger.info(\"Setting text to: \" + $1);\n");
+                        src.append("    log(\"Setting text to: \" + $1);\n");
+                        src.append("    $1  = \"--\" + $1 + \"--\"; \n");
                         src.append("}");
                         setTextMethod.insertBefore(src.toString());
                         return clazz.toBytecode();
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    e.printStackTrace(pw);
+                    System.out.println("Captured printStackTrace():\n" + sw);
+                    log("Exception: " + sw);
                 }
                 return classfileBuffer;
             }
